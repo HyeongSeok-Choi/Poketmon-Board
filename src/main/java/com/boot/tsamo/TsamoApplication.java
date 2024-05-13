60e68f7a0006becbc0547622b025f7982cdac4b5
black2890 <100751771+black2890@users.noreply.github.com>
2024-05-13 13:34:18 +0900

    Init

---
+++ src/main/java/com/boot/tsamo/TsamoApplication.java
@@ -0,0 +1,13 @@
+package com.boot.tsamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsamoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsamoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boot/tsamo/TsamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.tsamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsamoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
