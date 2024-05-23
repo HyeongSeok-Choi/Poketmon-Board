3dc71b190d232d8d24dd95cb11c8790bd64923f9
bumeruchi <jack1871@naver.com>
2024-05-23 15:32:43 +0900

    포켓몬스터

--- src/main/java/com/boot/tsamo/controller/LikesController.java
+++ src/main/java/com/boot/tsamo/controller/LikesController.java
@@ -28,13 +28,10 @@ public class LikesController {
         //뷰에서 받아온 정보에 로그인 아이디 대입
         likesDTO.setUserId(principal.getName());
 
-
         //좋아요 존재유무 확인(true라면 이미 존재, false라면 좋아요 없음)
        boolean checkLikes= likeService.checklikes(likesDTO);
 
         return ResponseEntity.ok().body(checkLikes);
-
-
     }
 
     @PostMapping(value = "/getCounting")
